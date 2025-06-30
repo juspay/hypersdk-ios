e136f23abefb49b525ccbd21e2597caa12a600c0
Balaganesh S <balaganesh@juspay.in>
2025-06-30 14:48:51 +0530

    Updated Package for version 2.2.3.5

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "JuspaySafeBrowser", url: "https://github.com/juspay/juspaysafebrowser-ios.git", .exact("0.1.87")),
-        .package(name: "HyperCore", url: "https://github.com/juspay/hypercore-ios.git", .exact("0.0.6")),
+        .package(name: "JuspaySafeBrowser", url: "https://github.com/juspay/juspaysafebrowser-ios.git", .exact("0.1.88")),
+        .package(name: "HyperCore", url: "https://github.com/juspay/hypercore-ios.git", .exact("0.0.10")),
     ],
     targets: [
         .binaryTarget(
             name: "HyperSDK",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.3/HyperSDK.zip",
-            checksum: "5f2c28032a8e77f212583e4683e70eaa01cd9dc653ab4f46ffa9e7c9a6f9c9b6"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.3.5/HyperSDK.zip",
+            checksum: "6c4e1ec0fefb2988525db754f585fe93cdf40a46785c20ef80ca726c3ae5982b"
         ),
         .target(
             name: "HyperSDKDependencies",
