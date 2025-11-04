bcc9a257c8f6f99de5cf34369f0919c118d90324
Balaganesh S <balaganesh@juspay.in>
2025-11-04 14:43:55 +0530

    Updated Package for version 2.2.4.15

--- Package.swift
+++ Package.swift
@@ -9,18 +9,23 @@ let package = Package(
     products: [
         .library(
             name: "HyperSDK",
-            targets: ["HyperSDK", "HyperSDKDependencies"]
+            targets: ["HyperSDK", "Airborne", "HyperSDKDependencies"]
         )
     ],
     dependencies: [
-        .package(name: "JuspaySafeBrowser", url: "https://github.com/juspay/juspaysafebrowser-ios.git", .exact("0.1.88")),
-        .package(name: "HyperCore", url: "https://github.com/juspay/hypercore-ios.git", .exact("0.0.10")),
+        .package(name: "JuspaySafeBrowser", url: "https://github.com/juspay/juspaysafebrowser-ios.git", .exact("0.1.93")),
+        .package(name: "HyperCore", url: "https://github.com/juspay/hypercore-ios.git", .exact("0.1.7"))
     ],
     targets: [
         .binaryTarget(
             name: "HyperSDK",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.4/HyperSDK.zip",
-            checksum: "5ef49e7d3d72db86b206ad42ceaf4a445fdfefd4d3853091f8ecad7418f453ae"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.4.15/HyperSDK.zip",
+            checksum: "c938d21e65d58be62e45414cb9253dae55673872a3306517b3d5965b1f9f7528"
+        ),
+        .binaryTarget(
+            name: "Airborne",
+            url: "https://public.releases.juspay.in/release/ios/airborne/0.3.2/Airborne.zip",
+            checksum: "4d07186fc30b23ede59d5dac2302fc764428b47b0c97de4d01d373ed230e2716"
         ),
         .target(
             name: "HyperSDKDependencies",
