a164c7cf5aab26e94dc1a5b9e06558988d22738d
Balaganesh S <balaganesh@juspay.in>
2026-07-31 13:10:09 +0530

    Updated Package for version 2.2.8.5

--- Package.swift
+++ Package.swift
@@ -9,28 +9,35 @@ let package = Package(
     products: [
         .library(
             name: "HyperSDK",
+            targets: ["HyperSDK", "AirborneSDK", "JuspaySafeBrowser", "HyperSDKDependencies"]
+        ),
+        .library(
+            name: "HyperSDKCore",
             targets: ["HyperSDK", "AirborneSDK", "HyperSDKDependencies"]
         )
     ],
     dependencies: [
-        .package(name: "JuspaySafeBrowser", url: "https://github.com/juspay/juspaysafebrowser-ios.git", .exact("0.1.94")),
         .package(name: "HyperCore", url: "https://github.com/juspay/hypercore-ios.git", .exact("1.0.6"))
     ],
     targets: [
         .binaryTarget(
             name: "HyperSDK",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.8.4/HyperSDK.zip",
-            checksum: "86ea80303d2bd0450ada337b987e5a48b270c463a5001c4da4d9767f0155eacc"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.8.5/HyperSDK.zip",
+            checksum: "3d90b46b617b7f7cd2970df73252fadab2cd5c46951e1b48bd728c7beeccbdda"
         ),
         .binaryTarget(
             name: "AirborneSDK",
             url: "https://public.releases.juspay.in/release/ios/airborne/0.37.0/Airborne.zip",
             checksum: "c46f082129688da7a1b8a37a5867b29f6b2e1104ab210d1f1904a94e9794b97a"
         ),
+        .binaryTarget(
+            name: "JuspaySafeBrowser",
+            url: "https://public.releases.juspay.in/release/ios/juspay-safe-browser/0.1.94/JuspaySafeBrowser.zip",
+            checksum: "8c11bb36469574e622e1541561da999dec38d462b3bd5d377287ba29ba0f1159"
+        ),
         .target(
             name: "HyperSDKDependencies",
             dependencies: [
-                .product(name: "JuspaySafeBrowser", package: "JuspaySafeBrowser"),
                 .product(name: "HyperCore", package: "HyperCore")
             ]
         )
